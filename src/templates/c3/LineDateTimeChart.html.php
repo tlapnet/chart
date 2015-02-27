45727e56d3f03aeba12c3f8d78085bfa5a59c0d8
Ludek Benedik <lb@ludekbenedik.com>
2015-02-27 10:49:05 +0100

    feature: Implement C3 LineDateTimeChart

---
+++ src/templates/c3/LineDateTimeChart.html.php
@@ -0,0 +1,61 @@
+<div id="<?php echo $chartId ?>" style="width: <?php echo $width ?>; height: <?php echo $height ?>;"></div>
+<script type="text/javascript">
+	var axis = {
+		x: {
+			type: 'timeseries',
+			tick: {
+				format: '<?php echo ($maxTime - $minTime) > 90000 ? '%d.%m %H:%M' : '%H:%M' ?>'
+			}
+		},
+		y : {
+			tick: {
+                format: function (d) { return d + '<?php echo $valueSuffix ?>'; }
+			}
+		}
+	};
+	var data = {
+		xFormat: '%Y-%m-%d %H:%M:%S',
+		//labels: true,
+		xs: {},
+		names: {},
+		colors: {},
+		columns: []
+	};
+
+	<?php $i = 0 ?>
+	<?php foreach ($series as $serie): ?>
+		<?php $i++ ?>
+
+		data.xs['data<?php echo $i ?>'] = 'x<?php echo $i ?>';
+		data.names['data<?php echo $i ?>'] = '<?php echo $serie->getTitle() ?>';
+
+		<?php if ($serie->getColor() !== null): ?>
+			data.colors['data<?php echo $i ?>'] = '<?php echo $serie->getColor() ?>';
+		<?php endif ?>
+
+		var columnX = ['x<?php echo $i ?>'];
+		var columnY = ['data<?php echo $i ?>'];
+
+		<?php foreach ($serie->getSegments() as $segment): ?>
+			columnX.push('<?php echo $segment->getDateTime()->format('Y-m-d H:i:s') ?>');
+			columnY.push(<?php echo $segment->getValue() ?>);
+		<?php endforeach ?>
+
+		data.columns.push(columnX);
+		data.columns.push(columnY);
+	<?php endforeach ?>
+
+	c3.generate({
+		bindto: '#<?php echo $chartId ?>',
+		grid: {
+			x: {
+				show: true
+			},
+			y: {
+				show: true
+			}
+		},
+		axis: axis,
+		data: data
+	});
+</script>
