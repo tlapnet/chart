5ad1c4adc4c5fc208824f3b5f56e86bbf1b669b4
Ludek Benedik <lb@ludekbenedik.com>
2015-03-20 16:12:23 +0100

    feature: Remove old LineDateChart and all unused classes

--- src/Segment/PieSegment.php
+++ src/Segment/PieSegment.php
@@ -6,6 +6,40 @@ namespace Tlapnet\Chart\Segment;
 /**
  * @author Ludek Benedik
  */
-class PieSegment extends BaseTitleValueSegment
+class PieSegment
 {
+	/** @var string */
+	private $title;
+
+	/** @var float */
+	private $value;
+
+
+	/**
+	 * @param string $title
+	 * @param float $value
+	 */
+	function __construct($title, $value)
+	{
+		$this->title = (string) $title;
+		$this->value = (float) $value;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+
+	/**
+	 * @return float
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
 }
